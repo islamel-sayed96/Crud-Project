d7edb9d03498e1270467dbb882b495a5cfdb3099
islamel-sayed96 <islamshawqi525@gmail.com>
2025-07-06 12:49:46 +0300

    add product

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,4 +4,16 @@ var productCategoryInput = document.getElementById('ProductCategory'); //  all i
 var productDescInput = document.getElementById('ProductDesc'); //  all input ...
 
 // ProductName >  id of input 
-console.log(productNameInput , productPriceInput , productCategoryInput , productDescInput);
+// console.log(productNameInput , productPriceInput , productCategoryInput , productDescInput);
+
+var productContainer = [] ;
+function addProduct(){
+    var product = {
+        name : productNameInput.value ,
+        price : productPriceInput.value ,
+        category : productCategoryInput.value ,
+        desc : productDescInput.value,
+    }
+    productContainer.push(product)
+    console.log(productContainer);
+}
